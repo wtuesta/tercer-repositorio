3a3bf2d3d9619b6a3929269a49d6496522f4ff9c
wtuesta <wtuesta@hotmail.com>
2023-10-28 12:37:04 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Home() {
+  const listItems = [1,2,3,4]
+
+const variableA = () => {
+  const addItem = 1;
+  const variableB = 3;
+  const variableC = 2;
+
+  return variableA*3 + variableC*variableB
+return (
+  <><div>Home</div><button onclick={addItem} /></>
+  )
+}
+
+  export default Home
